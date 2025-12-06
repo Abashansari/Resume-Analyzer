10fe356f47c2b344e4d30b37633a19942a06af35
Abash Ansari <ansariabash2004@gmail.com>
2025-12-06 14:30:17 +0530

    config mongoDb

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,1 +1,17 @@
-// MongoDB connection
+import moongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const mongoDbURL = process.env.MONGO_DB_URL
+
+const connectDB = async() =>{
+    try {
+        await moongoose.connect(mongoDbURL)
+        console.log("MongoDB is connected successfully !")
+
+    } catch (error) {
+        console.log("Error :", error.message)
+    }
+}
+export default connectDB
