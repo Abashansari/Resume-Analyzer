4d5fe4e7a2ef82339a082cb4210fa8402f067359
Abash Ansari <ansariabash2004@gmail.com>
2025-12-06 19:03:33 +0530

    used userRouter

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
-import express from "express";
+import express from "express"
+import userRouter from "./routes/userRoutes.js"
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/user', userRouter)
 
 export default app
