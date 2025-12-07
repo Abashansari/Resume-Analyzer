0c882f564766903d6271fdc7f0d9c1427eb51863
Abash Ansari <ansariabash2004@gmail.com>
2025-12-07 16:34:34 +0530

    used express.static()

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 import express from "express"
 import userRouter from "./routes/userRoutes.js"
+import resumeRouter from "./routes/resumeRoutes.js"
 
 const app = express()
 
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
 
 app.use('/user', userRouter)
+app.use('/resume', resumeRouter)
 
 export default app
